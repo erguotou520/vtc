a67b32298c0e35e00e53eb922431bca7303b1544
erguotou <erguotou525@gmail.com>
2017-04-26 17:17:07 +0800

    fix import and export issue

--- index.js
+++ index.js
@@ -1,62 +1,51 @@
 import { Cache, default as CacheTool } from './cache'
-import { default as _search } from './utils/search'
-import * as utils from './utils/utils'
-export default Object.assign({}, {
+import search from './utils/search'
+import {
+  merge,
+  clone,
+  param,
+  resolvePath,
+  isString,
+  isNumber,
+  isRegExp,
+  isBool,
+  isDate,
+  isArray,
+  isObject,
+  isFunction
+} from './utils/utils'
+export default {
   Cache,
   CacheTool,
-  search: _search
-}, utils)
-
-export { Cache, CacheTool }
-
-export function search (...args) {
-  return search.apply(args)
+  search,
+  merge,
+  clone,
+  param,
+  resolvePath,
+  isString,
+  isNumber,
+  isRegExp,
+  isBool,
+  isDate,
+  isArray,
+  isObject,
+  isFunction
 }
 
-export function merge (...args) {
-  return utils.merge.apply(args)
-}
-
-export function clone (...args) {
-  return utils.clone.apply(...args)
-}
-
-export function param (...args) {
-  return utils.param(args)
-}
-
-export function resolvePath (...args) {
-  return utils.resolvePath.apply(args)
-}
-
-export function isString (...args) {
-  return utils.isString.apply(args)
-}
-
-export function isNumber (...args) {
-  return utils.isNumber.apply(args)
-}
-
-export function isRegExp (...args) {
-  return utils.isRegExp.apply(args)
-}
-
-export function isBool (...args) {
-  return utils.isBool.apply(args)
-}
-
-export function isDate (...args) {
-  return utils.isDate.apply(args)
-}
-
-export function isArray (...args) {
-  return utils.isArray.apply(args)
-}
-
-export function isObject (...args) {
-  return utils.isObject.apply(args)
-}
-
-export function isFunction (...args) {
-  return utils.isFunction.apply(args)
+export {
+  Cache,
+  CacheTool,
+  search,
+  merge,
+  clone,
+  param,
+  resolvePath,
+  isString,
+  isNumber,
+  isRegExp,
+  isBool,
+  isDate,
+  isArray,
+  isObject,
+  isFunction
 }
